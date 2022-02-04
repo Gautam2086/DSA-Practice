f8761e89860df080b827226dea73c319d14842f8
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2022-02-05 01:59:01 +0530

    Time: 226 ms (27.59%), Space: 83.9 MB (25.67%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -1,6 +1,8 @@
 /*
-+1 -> 1
--1 -> 0
+Here the idea is to just consider the zero as -1 and take the prefix sum and maintain a hashmap where the key is the prefixSum and value is its index. Whenever we see the prefixSum inside the hashmap, then we can update our maxLength = max(maxLength, i - map.get(prefixSum)). otherwise we just put the prefixSum and index to the hashmap.
+
++1 -> when encounter 1
+-1 -> when encounter 0
 calculate Prefix sum
 TC: O(n)
 */
