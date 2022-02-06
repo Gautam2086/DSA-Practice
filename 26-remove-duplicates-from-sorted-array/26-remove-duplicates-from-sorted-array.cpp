9569ef09dee22ed74c0518c51dbe3e16c7fb3a16
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2022-02-06 23:09:35 +0530

    Time: 24 ms (25.63%), Space: 19.3 MB (5.81%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    // TC: O(n*logn) + O(n)    
+    // => n: traversing in array, log n: inserting in set, n for placing set ele in array front
+    // SC: O(n)
+    
     int removeDuplicates(vector<int>& nums) {
         set<int> s;
         
         for(int x: nums) 
             s.insert(x);
         
-        // for(int x: s) 
-        //     cout<<x<<" ";
+
         int n= s.size();
         
         int i= 0;
