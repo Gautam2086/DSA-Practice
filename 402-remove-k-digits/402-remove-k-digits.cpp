d4b1283fdd282b81ac40baab8cf73f0fccb80e55
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2022-02-19 15:15:23 +0530

    Time: 4 ms (83.57%), Space: 7.9 MB (18.39%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,30 +1,54 @@
 class Solution {
 public:
-// The first algorithm is straight-forward. Let's think about the simplest case: how to remove 1 digit from the number so that the new number is the smallest possible？ Well, one can simply scan from left to right, and remove the first "peak" digit; the peak digit is larger than its right neighbor. One can repeat this procedure k times, and obtain the first algorithm
+/* 
+-Our aim is to remove digit nums[i] starting from left where num[i]>nums[i+1]
+-Using stack (here used stack) we compare the top element the present element
+-If top element>present element we pop the top element from the stack (or string) and push the       present element
+-If more element are still left to be removed i.e k>0 we remove the elements from the end of the     string(stack)
+-We check if first element (character) is 0
+-if YES then we reverse the string and remove the elements from the end till no zeros are left at     the back of the string. Then we reverse the string again.  
+TC: O(n), SC: O(n)
+*/
     
     string removeKdigits(string num, int k) {
         
-        while(k>0)
+        stack<char> st;
+        
+        int i= 0;
+        for(; i<num.length() && k>0; i++)
         {
-            int n= num.size();
-            int i= 0;
-            
-            while(i<n-1 && num[i] <= num[i+1])
-                i++;
-            num.erase(i, 1);
+            while(k>0 && !st.empty() && num[i]-'0' < st.top()-'0') {
+                st.pop();
+                k--;
+            }
+            st.push(num[i]);
+        }
+        
+        while(!st.empty() && k>0)           // for edge cases like: num= "13579", k=3
+        {
+            st.pop();
             k--;
         }
         
-        // trim leading zeros
-        int i= 0;
-        while(i< num.length()-1 && num[i]=='0')
+        string ans= "";                     // Build ans string
+        while(!st.empty()) {                
+            ans= st.top() + ans;            // iterate in reverse fashion
+            st.pop();
+        }
+        
+        ans= ans + num.substr(i, num.length());   //append remaining elements(if any) of num in ans
+        
+        //trim leading zeros in case of 00200
+        i= 0;
+        while(i<ans.length()-1 && ans[i] == '0')
             i++;
         
-        num.erase(0, i);
+        ans.erase(0, i);
         
-        if(num == "")
+    
+        if(ans == "")
             return "0";
         else
-            return num;
+            return ans;
     }
 };
