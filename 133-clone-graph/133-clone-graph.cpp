188349f1aa6a9a2b8002a99163788634720077b0
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2022-02-24 01:44:04 +0530

    Time: 7 ms (73.40%), Space: 8.8 MB (59.70%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,65 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    
+    void edges(unordered_map<Node*, Node*>& mp) {
+        
+        for(auto u : mp) {
+            for(auto v : u.first->neighbors) {
+                u.second->neighbors.push_back(mp[v]);
+            }
+        }
+    }
+    
+    void dfs(Node* curr, set<Node*>& vis, unordered_map<Node*, Node*>& mp)
+    {
+        Node* clone= new Node(curr->val);
+        vis.insert(curr);
+        mp[curr]= clone;
+        
+        for(auto v: curr->neighbors)
+        {
+            if(vis.find(v) == vis.end()) 
+                dfs(v, vis, mp);
+        }
+    }
+    
+    Node* cloneGraph(Node* node) {
+        
+        if(node == NULL)
+            return NULL;
+        
+        if(node->neighbors.size() == 0) {            //if only one node present no neighbor
+            Node* clone= new Node(node->val);   
+            return clone;
+        }
+        
+        unordered_map<Node*, Node*> mp;
+        set<Node*> vis;
+        
+        dfs(node, vis, mp);
+        edges(mp);
+        
+        return mp[node];
+    }
+};
