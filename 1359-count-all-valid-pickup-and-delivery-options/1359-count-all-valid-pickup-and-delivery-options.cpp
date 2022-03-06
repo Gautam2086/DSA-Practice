c4a87e64404b6e8a7a5c56f7b7a6ee282fcc7ba8
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2022-03-07 00:02:22 +0530

    Time: 4 ms (44.03%), Space: 5.9 MB (53.14%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
-    // Formula: n! {Pickups} * 1*3*5*(2n-1) {Deliveries arrangement}
+/* Formula: n! {Pickups} * 1*3*5*(2n-1) {Deliveries arrangement} 
+
+https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/discuss/1823403/C%2B%2B-oror-Easy-To-Understand-oror-Full-Explanation-oror-Coimbanotrics
+    
+https://www.youtube.com/watch?v=GVWIUk5eID8
+*/
     int countOrders(int n) {
         
         long mod= 1e9+7;
