b7e4c79d14f7cc7c2209c8bd4ff5b29eb578179b
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2022-03-13 21:43:05 +0530

    Time: 3 ms (35.76%), Space: 6.3 MB (51.50%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    bool isValid(string s)
-    {
+    bool isValid(string s) {
+        
+        if(s.size() == 1)
+            return false;
+        
         stack<char> st;
-        
-        for(char c: s)
+        for(auto ch: s) 
         {
-            if(c=='(' || c=='{' || c=='[')
-                st.push(c);
-            else
-            {   
-                if(st.empty())  return false;
-                if(c==']' && st.top() !='[')    return false;
-                if(c=='}' && st.top() !='{')    return false;
-                if(c==')' && st.top() !='(')    return false;
+            if(ch == '(' || ch == '[' || ch == '{')
+                st.push(ch);
             
+            else
+            {
+                if(st.size() == 0)
+                    return false; 
+                if(ch==']' && st.top() != '[')    return false;
+                if(ch=='}' && st.top() != '{')    return false;
+                if(ch==')' && st.top() != '(')    return false;
+                
                 st.pop();
-            } 
+            }
+            
         }
+        if(st.size() == 0)
+            return true;
         
-        return st.empty();
+        return false;
     }
 };
