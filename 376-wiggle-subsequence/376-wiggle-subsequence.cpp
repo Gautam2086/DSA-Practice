d8c4f179582ee96f71a6f8ce1586817ab530167f
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2022-03-24 21:34:11 +0530

    Time: 4 ms (51.71%), Space: 7.1 MB (43.37%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -5,21 +5,19 @@ public:
         if(nums.size()==1)
             return nums.size();
         
-        int prev_diff= nums[1]-nums[0];
+        int inc= 1;
+        int dec= 1;
         
-        int count= (prev_diff!=0) ? 2:1;        // ignore one no. if any two are same
-        
-        for(int i=2; i<nums.size(); i++)
-        {
-            int curr_diff= nums[i]-nums[i-1];
-            
-            if((curr_diff > 0 && prev_diff <= 0) || (curr_diff < 0 && prev_diff >= 0))
-            {
-                count++;
-                prev_diff= curr_diff;
+        for(int i=1; i<nums.size(); i++) {
+            if(nums[i] > nums[i-1]) {
+                inc= dec+1;
             }
-            
+            else if(nums[i] < nums[i-1]) {
+                dec= inc+1;
+            }
+            // if(nums[i] == nums[i-1])       => do nothing with inc & dec, just ignore
         }
-        return count;
+        
+        return max(inc, dec);
     }
 };
