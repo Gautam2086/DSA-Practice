ae50fc55fa3d53f06b62af49d8e0172c55468415
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2022-04-23 20:04:31 +0530

    Time: 7 ms (39.83%), Space: 7.3 MB (16.54%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    map<string, string> mp;
+    unordered_map<string, string>mp;
+    int length = 6;
     
+    string hashing(string longUrl){
+        string hashCode = "";
+        
+        for(int i=0; i<length; i++){
+            int random = rand() % 3;
+            
+            if(random == 0)
+                hashCode += ('0' + rand() % 10);
+            else if(random == 1)
+                hashCode += ('a' + rand() % 26);
+            else
+                hashCode += ('A' + rand() % 26);
+        }
+        
+        if(mp.find(hashCode) !=mp.end())
+            return hashing(longUrl);
+        else{
+            mp[hashCode] = longUrl;
+            return hashCode;
+        }
+    }
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        if(!mp.count(longUrl))
-        {
-            string shortUrl= "http://tinyurl.com/" + to_string(mp.size());
-            mp[longUrl]= shortUrl;
-            mp[shortUrl]= longUrl;
-        }
-        return mp[longUrl];
+        string tinyUrl = "http://tinyurl.com/";
+        string hashCode = hashing(longUrl);
+        return tinyUrl + hashCode;
     }
-
+    
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return mp[shortUrl];
+        string hashCode = shortUrl.substr(shortUrl.size() - length, length);
+        return mp[hashCode];
     }
 };
-
-// Your Solution object will be instantiated and called as such:
-// Solution solution;
-// solution.decode(solution.encode(url));
