81b0c7e49ba21c07593033440a1e5a9a11b43890
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2022-04-28 22:06:47 +0530

    Time: 716 ms (17.24%), Space: 76.8 MB (10.82%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,48 +1,58 @@
 class Solution {
-public:
-    bool canReach(vector<vector<int>>& heights, int m, int n, int x, int y, int minDiff,                                   vector<vector<bool>>& visited)
-    {
-        if(x==m-1 && y==n-1)    return true;
-        if(visited[x][y] == 1)  return false;
+    vector<vector<int>> dirs = {
+                {-1,0},
+        {0,-1},         {0,1},
+                {1, 0}
+    };
+    
+    bool isValid(vector<vector<int>>& heights, int mx) {
+        queue<pair<int,int>> q;
+        int ROWS = heights.size();
+        int COLS = heights[0].size();
         
-        visited[x][y]= true;
+        vector<vector<bool>> seen(ROWS, vector<bool>(COLS, false));
+        q.push({0,0});
+        seen[0][0] = true;
+        while(!q.empty()) {
+            auto node = q.front();
+            q.pop();
+            int r = node.first;
+            int c = node.second;
+            if(r == ROWS - 1 && c == COLS - 1) return true;
+            for(auto& dir : dirs) 
+            {
+                int dr = dir[0] + r;
+                int dc = dir[1] + c;
+                if(dr < 0 || dr >= ROWS) continue;
+                if(dc < 0 || dc >= COLS) continue;
+                if(seen[dr][dc]) continue;
+                if(abs(heights[r][c] - heights[dr][dc]) > mx) continue;
+                
+                seen[dr][dc] = true;
+                q.push({dr, dc});
+            }
+        }
         
-        if (x - 1 >= 0 && abs(heights[x][y] - heights[x - 1][y]) <= minDiff)
-            if (canReach(heights, m, n, x - 1, y, minDiff, visited)) return true;
-        
-        if (y + 1 < n &&  abs(heights[x][y] - heights[x][y + 1]) <= minDiff) 
-            if (canReach(heights, m, n, x, y + 1, minDiff, visited)) return true;
-        
-        if (x + 1 < m &&  abs(heights[x][y] - heights[x + 1][y]) <= minDiff) 
-            if (canReach(heights, m, n, x + 1, y, minDiff, visited)) return true;
-        
-        if (y - 1 >= 0 && abs(heights[x][y] - heights[x][y - 1]) <= minDiff) 
-            if (canReach(heights, m, n, x, y - 1, minDiff, visited)) return true;
-
         return false;
     }
-    bool isValid(int mid, vector<vector<int>>& heights, int m, int n) {
-        vector<vector<bool>> vis(m, vector<bool>(n, false));
-        return canReach(heights, m, n, 0, 0, mid, vis);
-    }
     
+public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        
-        int m= heights.size();                  // rows 
-        int n= heights[0].size();               // cols
-        int lo= 0;
-        int hi= 1000001;
-        int ans= 0;
-        
-        while(lo<=hi)
-        {
-            int mid= lo+(hi-lo)/2;
+
+        int L = 0;
+        int R = INT_MAX;
+        int best = INT_MAX;
+        while(L <= R) {
+            int M = L + (R-L) / 2;
             
-            if(isValid(mid, heights, m, n))
-                hi= mid-1, ans= mid;
-            else
-                lo= mid+1;
+            if(isValid(heights, M)) {
+                R = M - 1;
+                best = min(best, M);
+            } else {
+                L = M + 1;
+            }
         }
-        return ans;
+        
+        return best;
     }
 };
