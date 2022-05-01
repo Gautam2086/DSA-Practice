1dbdfc9d81c9c2f73e01b81006c0f379b63dd2f6
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2022-05-01 17:19:18 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (97.67%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -3,21 +3,44 @@ public:
     // TC: O(m+n)
     // SC: O(m+n)
     bool backspaceCompare(string s, string t) {
-        stack<char> st1, st2;
+        int i= s.length()-1;
+        int j= t.length()-1;
         
-        for(auto ch: s) {
-            if(ch == '#' && !st1.empty())
-                st1.pop();
-            else if(ch != '#')
-                st1.push(ch);
+        int sSkips= 0;
+        int tSkips= 0;
+        
+        while(i>=0 or j>=0) 
+        {
+            while(i>=0)
+            {
+                if(s[i] == '#')
+                    sSkips++;
+                else if(sSkips > 0)
+                    sSkips--;
+                else
+                    break;
+                i--;
+            }
+            while(j>=0)
+            {
+                if(t[j] == '#')
+                    tSkips++;
+                else if(tSkips > 0)
+                    tSkips--;
+                else
+                    break;
+                j--;
+            }
+            
+            if(i>=0 and j>=0 and s[i]!=t[j])
+                return false;
+            
+            if((i>=0) != (j>=0))
+                return false;
+            
+            i--;    j--;
         }
-        for(auto ch: t) {
-            if(ch == '#' && !st2.empty())
-                st2.pop();
-            else if(ch != '#')
-                st2.push(ch);
-        }
-
-        return st1==st2;
+        
+        return true;
     }
 };
