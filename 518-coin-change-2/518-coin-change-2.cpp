d25ca09d59e2355e9c3af1f5d03ad90d98aa248b
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2022-05-22 15:58:10 +0530

    Time: 52 ms (32.27%), Space: 19 MB (15.29%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    int helper(int i, vector<int>& coins, int amount, vector<vector<int>>& dp) {
+        
+        if(amount == 0)                                     // Base case when amt==0
+            return 1;   
+        
+        if(i>=coins.size() || amount<0)                     // we aint able to make upto amt        
+            return 0;                                       // so we need INT_MAX coins
+        
+        if(dp[i][amount] != -1)
+            return dp[i][amount];
+        
+        int ways1= 0;
+        int ways2= 0; 
+        
+        if(coins[i] <= amount)                               // check of coin[i] <= amt
+            ways1 = helper(i, coins, amount-coins[i], dp); 
+
+        ways2 = helper(i+1, coins, amount, dp);              // this optn is always available
+        
+        return dp[i][amount]= ways1+ways2;
+    }
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, -1));
+        
+        return helper(0, coins, amount, dp);
+    }
+};
