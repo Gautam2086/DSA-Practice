2e487b5242153bcf88b911b6d376d698de9e91ab
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2022-05-23 21:16:55 +0530

    Time: 383 ms (50.85%), Space: 9.8 MB (83.54%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,48 +1,24 @@
 class Solution {
 public:
-    int dp[601][101][101]= {};
-    int helper(vector<pair<int, int>>& v, int idx, int ones, int zeros) 
+    int findMaxForm(vector<string>& strs, int m, int n) 
     {
-        // BASE CASES
-        // 1. if we have reched the end of our array then we dont have any options left
-        // 2. if we have no available zeros or ones to use then also we dont have any options               left
-        if(idx == v.size() or (ones == 0 and zeros == 0))
-            return 0;
+        int i, j;
+        vector<vector<int>> dp(m+1 , vector<int>(n+1));
         
-        if(dp[idx][ones][zeros] != -1)
-            return dp[idx][ones][zeros];
-        
-        if(v[idx].first > ones or v[idx].second > zeros)
-            return helper(v, idx+1, ones, zeros);
-        
-        // now at this point we have two paths to explore, we want to know that what is the              answer going to be if i 
-        // use this pair 
-        // or if i leave this pair
-        int include = 1 + helper(v, idx+1, ones-v[idx].first, zeros-v[idx].second);
-        int exclude = 0 + helper(v, idx+1, ones, zeros);
-        
-        // now since we know the our answers for both of our decisions then we can choose the            one which yeilds the max result
-        return dp[idx][ones][zeros]= max(include, exclude);
-    }
-    int findMaxForm(vector<string>& strs, int m, int n) {
-        
-        memset(dp ,-1, sizeof dp);
-        // create vector pair to store no. of zeros & ones or ith string
-        vector<pair<int, int>> v;
-        for(auto i: strs) {
-            int one= 0, zero= 0;
+        for(auto &s:strs)
+        {
+            int x = count(s.begin(),s.end(),'1');
+            int y = s.size()-x;
             
-            for(auto j: i) {
-                if(j == '1')
-                    one++;
-                else
-                    zero++;
+            for(i=m; i>=y; i--)
+            {
+                for(j=n; j>=x; j--)
+                {
+                    dp[i][j] = max(dp[i][j], 1 + dp[i-y][j-x]);
+                }
             }
-            v.push_back({one, zero});
         }
         
-        int ans= helper(v, 0, n, m);
-        
-        return ans;
+        return dp[m][n];
     }
 };
